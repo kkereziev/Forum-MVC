5f48ab818e2dc2357bd78338e27acd079309067c
krumeto97 <krumeto97@abv.bg>
2020-06-30 14:57:52 +0300

    making services

---
+++ ForumProject/ForumProject.Data/Interfaces/IForum.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ForumProject.Data.Models;
+
+namespace ForumProject.Data.Interfaces
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+
+        IEnumerable<Forum> GetAll();
+
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+
+        Task Delete(int forumId);
+
+        Task UpdateForumTitle(int forumId, string newTitle);
+
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+    }
+}
