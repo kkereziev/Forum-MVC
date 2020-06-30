a8ebd39bb87f4cdebbb7f27203e35362d4bbb571
krumeto97 <krumeto97@abv.bg>
2020-06-30 13:54:03 +0300

    First commit

---
+++ ForumProject/ForumProject.Data/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ForumProject.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string UserDescription { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public int Rating { get; set; }
+        public DateTime MemberSince { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ForumProject/ForumProject.Data/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumProject.Data.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        
+        public DateTime Created { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual Forum Forum { get; set; }
+
+
+    }
+}
